3003460f9c52d8e0b7022374fe912cf8b3747e4d
caisui <rincaisui@gmail.com>
2010-05-21 21:44:54 +0900

    * バックページを表示項目top を bottom 近辺で選択になるように修正 * top 項目の highlight が 残らないように修正

--- plugin/scroll-completer.js
+++ plugin/scroll-completer.js
@@ -5,23 +5,21 @@
 	function scrollCompleter(dir){
 		let list = commandline._completionList;
 		let comp = commandline._completions;
-		if(list._selIndex>0)
+		if(list._selIndex>=0)
 			list._getCompletion(list._selIndex).removeAttribute("selected");
 		
 		if(dir){
 			var offset = list._endIndex - 1;
 			var index  = offset - 1;
-			var tab = false;
 		}else{
+			index = list._startIndex - 1;
 			let count = list._endIndex - list._startIndex - 1;
-			offset = Math.max(list._selIndex - count,0);
-			index = offset + 1;
-			tab = true;
+			offset = Math.max(index - count,0);
 		}
 		list._fill(offset);
 		list._selIndex = -1;
 		comp.selected = index;
-		comp.tab(tab);
+		comp.tab(false);
 	}
 
 	mappings.addUserMap([modes.COMMAND_LINE],nextMap,"scroll page",function(){ scrollCompleter(true); });
