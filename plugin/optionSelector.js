4968f2ff8357ef55c80517006ae59fe263437ab7
caisui <rincaisui@gmail.com>
2010-05-24 23:32:56 +0900

    * optgroup で グループ化 * autoShowComplete を options.optionselectautoshow に 変更 * 独自絞り込みを hint の matcher で 絞り込みに変更

--- plugin/optionSelector.js
+++ plugin/optionSelector.js
@@ -1,8 +1,11 @@
+// vim: set et :
 //optionSelector.js
 (function(){
-  const autoShowComplete = 1;
+  const key = "optionselectautoshow";
+  options.add([key], "select completer auto show", "boolean", true);
   //Register.add("s",function(){ selectInput();return "";});
   //mappings.addUserMap([modes.INSERT],["<C-s>"],"select completer",function() selectInput());
+
   (function () {
     let map = mappings.get(modes.INSERT, "<C-i>");
     if(!map) return;
@@ -13,7 +16,7 @@
       else action.call(this, arguments);
     };
   })();
-  
+
   function has(a,b) (b in a)
   function is(a,b) a instanceof b
   function selectInput(elem) {
@@ -29,21 +32,38 @@
       evt.initEvent("change",true,true);
       elem.dispatchEvent(evt);
     },{
-      completer:function(context){
+      completer:function(context) {
+        if (elem.options.length === 0) return;
+
         context.filter = context.filter.trim();
-        if(has(window,"migemo")){
-          let re = window.migemo.getRegExp(context.filter,"i");
-          context.match = function(str) re.test(str); 
-        }else{
-          context.match = function(str) str.indexOf(this.filter)>=0;
+
+        let matcher = hints._hintMatcher(context.filter);
+        context.match = matcher;
+
+        let parent = elem;
+        let aContext = context;
+        let list = [[" ",""]];
+        for (let opt in util.Array.itervalues(elem.options)) {
+          if (parent !== (parent = opt.parentNode)) {
+            aContext.completions = list;
+            aContext = CompletionContext(context, context.contextList.length, 0);
+            let label = parent.label || "";
+            aContext.title[0] = label;
+            aContext.match = matcher(label) ? function() true : matcher;
+            list = [];
+            context.contextList.push(aContext);
+          }
+          if (!parent.disabled && !opt.disabled)
+            list.push(_getItem(opt));
         }
-        context.completions = [[" ",""]].concat([ [<>{i}:{let(s=opt.parentNode.label) s? (s+"/"):""}{opt.text}</>,opt.value]
-          for([i,opt] in Iterator(elem.options)) if(!opt.disabled && !opt.parentNode.disabled)
-        ]);
-      },onCancel:function(){
+        aContext.completions = list;
+
+        function _getItem(opt) [opt.index + ": " + opt.text, opt.value]
+      },
+      onCancel: function () {
         window.setTimeout(function() elem.focus(),0);
       }
     });
-    autoShowComplete&&commandline._tabTimer.tell({shitKey:false});
+    options[key] && commandline._tabTimer.tell({shitKey: false});
   }
 })();
