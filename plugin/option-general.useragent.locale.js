6d982b48f76dd9b0ad9e0a13a7d31a7d75a9b2f4
caisui <rincaisui@gmail.com>
2010-06-06 02:17:37 +0900

    タブをスペースに変更

--- plugin/option-general.useragent.locale.js
+++ plugin/option-general.useragent.locale.js
@@ -1,16 +1,16 @@
 (function () {
-	const name = "general.useragent.locale";
-	const opName = name.replace(/\./g, "_");
+    const name = "general.useragent.locale";
+    const opName = name.replace(/\./g, "_");
 
-	options.remove(opName);
-	options.add([opName], "switch locale", "string", 
-	options._loadPreference("general.useragent.locale", null, true),
-	{
-		completer: function (context) [["ja", "japan"], ["en-US", "english"]],
-		getter: function () options.getPref(name),
-		setter: function (value) {
-			options.setPref(name, value);
-			return value;
-		}
-	});
+    options.remove(opName);
+    options.add([opName], "switch locale", "string", 
+    options._loadPreference("general.useragent.locale", null, true),
+    {
+        completer: function (context) [["ja", "japan"], ["en-US", "english"]],
+        getter: function () options.getPref(name),
+        setter: function (value) {
+            options.setPref(name, value);
+            return value;
+        }
+    });
 })();
