8f8c21e5583c313cb44e227ec89020b4d29a31ee
caisui <rincaisui@gmail.com>
2011-03-30 00:01:16 +0900

    [hintchars-ex] アルゴリズムの最適化

--- plugin/hintchars-ex.js
+++ plugin/hintchars-ex.js
@@ -1,6 +1,6 @@
 // vim: set fdm=marker  et ts=4 sw=4:
 var INFO = //{{{
-<plugin name="hintchars-ex" version="0.0.1"
+<plugin name="hintchars-ex" version="0.0.2"
         href="http://github.com/caisui/vimperator/blob/master/plugin/hintchars-ex.js"
         summary="Hint Chars Ex"
         xmlns="http://vimperator.org/namespaces/liberator">
@@ -15,55 +15,68 @@ var INFO = //{{{
 </plugin>; //}}}
 
 (function () {
+    let cache = {};
+
+    function getProgression(r, n) {
+        let table = cache[r] || (cache[r] = {});
+        return table[n] || (table[n] = (Math.pow(r, n) - 1) / (r - 1));
+    }
+    function iterProgression(r) {
+        let cur = 1, pre;
+        for (let i in step(2)) {
+            pre = cur;
+            cur = getProgression(r, i);
+            yield [i, pre, cur];
+        }
+    }
+    function step(i) {
+        if (i === void(0)) i = 0;
+        while (1) yield i++;
+    }
+
+    let logBase = {};
+    function Log(base, num) {
+        base = logBase[base] || (logBase[base] = Math.log(base));
+        return Math.log(num) / base;
+    }
+
     hints._chars2num = function(chars) {
         const self = this;
         let hintchars = options.hintchars;
         let base = hintchars.length;
         let num = Array.reduce(chars, function(n, c) n * base + hintchars.indexOf(c), 0);
-        let digit = 1;
 
-        for (var i = 1, j = chars.length; i < j; ++i)
-            num += (digit *= base);
+        num += getProgression(base, chars.length);
 
-        return num + 1;
+        return num;
     };
     hints._num2chars = function(num) {
-        --num;
         let hintchars = options.hintchars;
         let base = hintchars.length;
-        let digit = 1;
-        for (let i = base; num >= i; ++digit, i *= base) {
-            num -= i;
-        }
+        let digit;
+        for (let [i, j, k] in iterProgression(base))
+            if (num < k) {
+                num -= j;
+                digit = i;
+                break;
+            }
+
+        // xxx: たぶんあってるが、まるめ誤差とMath.logのコストが心配なので
+        //digit = Math.floor(Log(base, (base - 1) * num + 1));
+        //num -= getProgression(base, digit++);
+
         let chars = "";
-        do {
+        while (num > 0) {
             chars = hintchars[num % base] + chars;
             num = Math.floor(num / base);
             --digit;
-        } while (num > 0);
-        return Array(digit + 1).join(hintchars[0]) + chars;
-    };
-    hints._checkUnique = function () {
-        let num = this._hintNumber;
-        let vnum = this._validHints.length;
-        if (num == 0)
-            return;
-        liberator.assert(num <= vnum);
-
-        let maxnum = this._chars2num(this._num2chars(num) + options.hintchars[0]);
-
-        if (num > 0 &&  maxnum <= vnum) {
-            let timeout = options["hinttimeout"];
-            if (timeout > 0)
-                this._activeTimeout = this.setTimeout(function () { this._processHints(true); }, timeout);
         }
-        else
-            this._processHints(true);
+        return Array(digit).join(hintchars[0]) + chars;
     };
 
     this.onUnload = function () {
         delete this.onUnload;
-        ["_chars2num", "_num2chars", "_checkUnique"]
+        ["_chars2num", "_num2chars"]
             .forEach(function (name) delete hints[name]);
     };
 }).call(this);
